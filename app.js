d3433aff5cfafe585136d25b3c522ca938e80974
daniyalamjad5144 <157717368+daniyalamjad5144@users.noreply.github.com>
2024-05-07 03:32:42 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+var num1 = +prompt("Enter value 1")
+var num2 = +prompt("Ener value 2")
+
+ var add = num1 + num2
+ var subtract = num1 - num2
+ var multiply = num1 * num2
+ var division = num1 / num2
+ var modulus = num1 % num2
+
+ document.write("Addition is : " +add +"<br>")
+ document.write("Subtraction is : " +subtract +"<br>")
+ document.write("Multiplication is : " +multiply +"<br>")
+ document.write("Division is : " +division +"<br>")
+ document.write("Modulus is : " +modulus +"<br>")
